3c9a7f01b64395cefc75fad5e0795811685f2421
Marcin Smoła <Marcinure1@gmail.com>
2021-09-21 13:11:27 +0200

    Search bar, basic course styling

--- src/views/coursesView.js
+++ src/views/coursesView.js
@@ -1,11 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledCourseList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row wrap;
+`;
+const StyledCourse = styled.li`
+  display: block;
+  flex-grow: 1;
+  min-width: 150px;
+  height: 250px;
+`;
 
 const CoursesView = () => {
   return (
-    <ul>
-      <li>Kurs 1</li>
-      <li>Kurs 2</li>
-    </ul>
+    <>
+      <div>Search</div>
+      <StyledCourseList>
+        <StyledCourse>Kurs 1</StyledCourse>
+        <StyledCourse>Kurs 2</StyledCourse>
+        <StyledCourse>Kurs 3</StyledCourse>
+        <StyledCourse>Kurs 4</StyledCourse>
+        <StyledCourse>Kurs 5</StyledCourse>
+        <StyledCourse>Kurs 6</StyledCourse>
+      </StyledCourseList>
+    </>
   );
 };
 
